7bf8eabed469656690d6ebce882ccd656c6f71e4
Juan Camilo Bages <jcbages@outlook.com>
2015-10-21 19:11:59 -0500

    hiddel files removed

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,3 @@
 (function () {
-	var chart = c3.generate({
-		data: {
-			columns: [
-				['Chelsea', 60, 400, 200, 800, 300, 200],
-				['Arsenal', 260, 200, 280, 400, 300, 100]
-			],
-			colors: {
-				'Chelsea': '#2E62C7',
-				'Arsenal': '#ED1C24'
-			},
-			type: 'area-spline'
-		},
-		axis: {
-			y: {
-				show: false
-			},
-			y2: {
-				show: false
-			},
-			x: {
-				show: false,
-			}
-		}
-	});
-
-	var actualBg = 0;
-	var bg = document.getElementById('bg');
-	var it = setInterval(function () {
-		actualBg = (actualBg + 1) % 4;
-		bg.className = "bg" + actualBg;
-	}, 8200);
+	
 })();
